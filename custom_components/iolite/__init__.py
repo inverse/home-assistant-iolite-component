8809d23c7ba059a46c902c2e3bc72731d98121a7
Malachi Soord <inverse.chi@gmail.com>
2021-10-17 22:51:36 +0200

    Refactor into DataUpdateCoordinator (#6)
    * Tidy
    
    * Bump
    
    * Fix up
    
    * Rename
    
    * Further

--- custom_components/iolite/__init__.py
+++ custom_components/iolite/__init__.py
@@ -1,21 +1,120 @@
-from homeassistant import config_entries, core
+import logging
+import time
+from datetime import timedelta
+from typing import Any, Dict
 
-from .const import DOMAIN
+from aiohttp import ClientSession
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_CODE, CONF_NAME, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.storage import Store
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from iolite_client.client import Client
+from iolite_client.oauth_handler import AsyncOAuthHandler
+
+from .const import DOMAIN, STORAGE_KEY, STORAGE_VERSION
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS = ["climate"]
 
 
-async def async_setup_entry(
-    hass: core.HomeAssistant, entry: config_entries.ConfigEntry
-) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up platform from a ConfigEntry."""
-    # Forward the setup to the climate platform
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "climate")
+    hass.data.setdefault(DOMAIN, {})
+    username: str = entry.data[CONF_USERNAME]
+    password: str = entry.data[CONF_PASSWORD]
+    name: str = entry.data[CONF_NAME]
+    code: str = entry.data[CONF_CODE]
+
+    websession = async_get_clientsession(hass)
+
+    coordinator = IoliteDataUpdateCoordinator(
+        hass, websession, username, password, name, code
     )
 
+    await coordinator.async_config_entry_first_refresh()
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+
     return True
 
 
-async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
-    """Set up the component from yaml configuration."""
-    hass.data.setdefault(DOMAIN, {})
-    return True
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
+
+
+async def get_sid(code: str, name: str, oauth_handler: AsyncOAuthHandler, store: Store):
+    """Get SID."""
+    access_token = await store.async_load()
+    if access_token is None:
+        _LOGGER.debug("No access token in storage, requesting")
+        access_token = await oauth_handler.get_access_token(code, name)
+        expires_at = time.time() + access_token["expires_in"]
+        access_token.update({"expires_at": expires_at})
+        await store.async_save(access_token)
+
+    if access_token["expires_at"] < time.time():
+        _LOGGER.debug("Access token expired, refreshing")
+        refreshed_token = await oauth_handler.get_new_access_token(
+            access_token["refresh_token"]
+        )
+        await store.async_save(refreshed_token)
+        token = refreshed_token["access_token"]
+    else:
+        token = access_token["access_token"]
+
+    _LOGGER.debug("Fetched access token")
+
+    return await oauth_handler.get_sid(token)
+
+
+class IoliteDataUpdateCoordinator(DataUpdateCoordinator[Dict[str, Any]]):
+    """Class to manage fetching IOLITE data."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        web_session: ClientSession,
+        username: str,
+        password: str,
+        pairing_name: str,
+        code: str,
+    ):
+        """Initialiser."""
+        self.hass = hass
+        self.web_session = web_session
+        self.username = username
+        self.password = password
+        self.pairing_name = pairing_name
+        self.code = code
+        self.client = None
+
+        update_interval = timedelta(seconds=30)
+        super().__init__(hass, _LOGGER, name=DOMAIN, update_interval=update_interval)
+
+    async def _async_update_data(self) -> dict[str, Any]:
+        store = self.hass.helpers.storage.Store(STORAGE_VERSION, STORAGE_KEY)
+
+        # Get SID
+        oauth_handler = AsyncOAuthHandler(
+            self.username, self.password, self.web_session
+        )
+        sid = await get_sid(self.code, self.pairing_name, oauth_handler, store)
+
+        self.client = Client(sid, self.username, self.password)
+        await self.client.async_discover()
+
+        rooms = {}
+        for room in self.client.discovered.get_rooms():
+            rooms[room.identifier] = room
+
+        return rooms
--- custom_components/iolite/climate.py
+++ custom_components/iolite/climate.py
@@ -1,33 +1,31 @@
 """Support for IOLITE heating."""
 
 import logging
-import time
 from typing import Any
 
 from homeassistant import config_entries
 from homeassistant.components.climate import ClimateEntity
 from homeassistant.components.climate.const import (
     HVAC_MODE_HEAT,
+    HVAC_MODE_OFF,
     SUPPORT_TARGET_TEMPERATURE,
 )
-from homeassistant.const import (
-    ATTR_TEMPERATURE,
-    CONF_CODE,
-    CONF_NAME,
-    CONF_PASSWORD,
-    CONF_USERNAME,
-    TEMP_CELSIUS,
-)
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-from homeassistant.helpers.storage import Store
+from homeassistant.const import ATTR_TEMPERATURE, TEMP_CELSIUS
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from iolite_client.client import Client
-from iolite_client.entity import RadiatorValve
-from iolite_client.oauth_handler import AsyncOAuthHandler
+from iolite_client.entity import RadiatorValve, Room
 
-from .const import DOMAIN, STORAGE_KEY, STORAGE_VERSION
+from . import IoliteDataUpdateCoordinator
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+
+
+SUPPORTED_HVAC_MODES = [
+    # HVAC_MODE_OFF,
+    # HVAC_MODE_HEAT
+]
 
 SUPPORT_FLAGS = SUPPORT_TARGET_TEMPERATURE
 
@@ -39,74 +37,45 @@ async def async_setup_entry(
 ):
     """Setup from config entry."""
 
-    config = config_entry.data
-    web_session = async_get_clientsession(hass)
-
-    username = config[CONF_USERNAME]
-    password = config[CONF_PASSWORD]
-
-    store = hass.helpers.storage.Store(STORAGE_VERSION, STORAGE_KEY)
-
-    # Get SID
-    oauth_handler = AsyncOAuthHandler(username, password, web_session)
-    sid = await get_sid(config[CONF_CODE], config[CONF_NAME], oauth_handler, store)
-
-    client = Client(sid, username, password)
-    await client.async_discover()
+    coordinator: IoliteDataUpdateCoordinator = hass.data[DOMAIN][config_entry.entry_id]
 
     # Map radiator valves
     devices = []
-    for room in client.discovered.get_rooms():
+    for room in coordinator.data.values():
         for device in room.devices.values():
             if isinstance(device, RadiatorValve):
-                devices.append(RadiatorValveEntity(device, client))
+                devices.append(
+                    RadiatorValveEntity(coordinator, device, coordinator.client)
+                )
 
     for device in devices:
         _LOGGER.info(f"Adding {device}")
 
-    async_add_entities(devices, update_before_add=True)
+    async_add_entities(devices)
 
 
-async def get_sid(code: str, name: str, oauth_handler: AsyncOAuthHandler, store: Store):
-    """Get SID."""
-    access_token = await store.async_load()
-    if access_token is None:
-        _LOGGER.debug("No access token in storage, requesting")
-        access_token = await oauth_handler.get_access_token(code, name)
-        expires_at = time.time() + access_token["expires_in"]
-        access_token.update({"expires_at": expires_at})
-        await store.async_save(access_token)
-
-    if access_token["expires_at"] < time.time():
-        _LOGGER.debug("Access token expired, refreshing")
-        refreshed_token = await oauth_handler.get_new_access_token(
-            access_token["refresh_token"]
-        )
-        await store.async_save(refreshed_token)
-        token = refreshed_token["access_token"]
-    else:
-        token = access_token["access_token"]
-
-    _LOGGER.debug("Fetched access token")
-
-    return await oauth_handler.get_sid(token)
-
-
-class RadiatorValveEntity(ClimateEntity):
+class RadiatorValveEntity(CoordinatorEntity, ClimateEntity):
     """Map RadiatorValue to Climate entity."""
 
     _attr_temperature_unit: str = TEMP_CELSIUS
     _attr_target_temperature_step: float = 0.5
-    _attr_hvac_modes: list = [HVAC_MODE_HEAT]
+    _attr_hvac_modes: list = SUPPORTED_HVAC_MODES
     _attr_supported_features: int = SUPPORT_FLAGS
 
-    def __init__(self, valve: RadiatorValve, client: Client):
+    def __init__(self, coordinator, valve: RadiatorValve, client: Client):
         """Initialize the valve."""
+        super().__init__(coordinator)
         self.valve = valve
         self.client = client
         self._attr_unique_id = valve.identifier
         self._attr_min_temp = 0
         self._attr_max_temp = 30
+        self._attr_name = self.valve.name
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, self._attr_unique_id)},
+            "name": self._attr_name,
+            "manufacturer": self.valve.manufacturer,
+        }
         self._update_state()
 
     async def async_set_temperature(self, **kwargs: Any) -> None:
@@ -116,32 +85,26 @@ class RadiatorValveEntity(ClimateEntity):
             return
 
         await self.client.async_set_temp(self.valve.identifier, temperature)
+        await self.coordinator.async_request_refresh()
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        self._update_state()
+        self.async_write_ha_state()
 
     @property
     def hvac_mode(self):
-        """Return hvac operation."""
+        """Return hvac target hvac state."""
+        if self._attr_current_temperature == self._attr_target_temperature:
+            return HVAC_MODE_OFF
+
         return HVAC_MODE_HEAT
 
-    async def async_update(self) -> None:
-        """Retrieve latest state."""
-        await self.client.async_discover()
-
-        matched = self.client.discovered.find_device_by_identifier(
-            self.valve.identifier
-        )
-
-        if not matched:
-            _LOGGER.warn(f"Failed to resolve RadiatorValue for {self.valve.identifier}")
-            return
-
-        self.valve = matched
-        self._update_state()
-
     def _update_state(self):
-        self._attr_name = self.valve.name
-        self._attr_current_temperature = self.valve.current_env_temp
-        self._attr_device_info = {
-            "identifiers": {(DOMAIN, self._attr_unique_id)},
-            "name": self._attr_name,
-            "manufacturer": self.valve.manufacturer,
-        }
+        room: Room = self.coordinator.data[self.valve.place_identifier]
+        self._attr_current_temperature = room.devices[
+            self.valve.identifier
+        ].current_env_temp
+        if room.heating:
+            self._attr_target_temperature = room.heating.target_temp

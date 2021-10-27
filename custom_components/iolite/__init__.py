2471946352f5bbca3009d87c5165147ebf02b591
Malachi Soord <inverse.chi@gmail.com>
2021-10-27 23:21:53 +0200

    Validate config_flow (#7)
    * Init validation
    
    * Missing bits
    
    * Readability
    
    * Tidy
    
    * Remove tests
    
    * Fix up wiring

--- custom_components/iolite/__init__.py
+++ custom_components/iolite/__init__.py
@@ -5,7 +5,7 @@ from typing import Any, Dict
 
 from aiohttp import ClientSession
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_CODE, CONF_NAME, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.storage import Store
@@ -25,14 +25,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     username: str = entry.data[CONF_USERNAME]
     password: str = entry.data[CONF_PASSWORD]
-    name: str = entry.data[CONF_NAME]
-    code: str = entry.data[CONF_CODE]
 
-    websession = async_get_clientsession(hass)
+    web_session = async_get_clientsession(hass)
 
-    coordinator = IoliteDataUpdateCoordinator(
-        hass, websession, username, password, name, code
-    )
+    coordinator = IoliteDataUpdateCoordinator(hass, web_session, username, password)
 
     await coordinator.async_config_entry_first_refresh()
     hass.data[DOMAIN][entry.entry_id] = coordinator
@@ -52,15 +48,9 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return unload_ok
 
 
-async def get_sid(code: str, name: str, oauth_handler: AsyncOAuthHandler, store: Store):
+async def get_sid(oauth_handler: AsyncOAuthHandler, store: Store):
     """Get SID."""
     access_token = await store.async_load()
-    if access_token is None:
-        _LOGGER.debug("No access token in storage, requesting")
-        access_token = await oauth_handler.get_access_token(code, name)
-        expires_at = time.time() + access_token["expires_in"]
-        access_token.update({"expires_at": expires_at})
-        await store.async_save(access_token)
 
     if access_token["expires_at"] < time.time():
         _LOGGER.debug("Access token expired, refreshing")
@@ -86,16 +76,12 @@ class IoliteDataUpdateCoordinator(DataUpdateCoordinator[Dict[str, Any]]):
         web_session: ClientSession,
         username: str,
         password: str,
-        pairing_name: str,
-        code: str,
     ):
         """Initialiser."""
         self.hass = hass
         self.web_session = web_session
         self.username = username
         self.password = password
-        self.pairing_name = pairing_name
-        self.code = code
         self.client = None
 
         update_interval = timedelta(seconds=30)
@@ -108,7 +94,7 @@ class IoliteDataUpdateCoordinator(DataUpdateCoordinator[Dict[str, Any]]):
         oauth_handler = AsyncOAuthHandler(
             self.username, self.password, self.web_session
         )
-        sid = await get_sid(self.code, self.pairing_name, oauth_handler, store)
+        sid = await get_sid(oauth_handler, store)
 
         self.client = Client(sid, self.username, self.password)
         await self.client.async_discover()
--- custom_components/iolite/config_flow.py
+++ custom_components/iolite/config_flow.py
@@ -1,14 +1,21 @@
 """Config flow for IOLITE."""
+import logging
+import time
 from typing import Any, Dict, Optional
 
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_CODE, CONF_NAME, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from iolite_client.oauth_handler import AsyncOAuthHandler
 
-from .const import DOMAIN
+from .const import DOMAIN, STORAGE_KEY, STORAGE_VERSION
 
-SCHEMA = vol.Schema(
+_LOGGER = logging.getLogger(__name__)
+
+AUTH_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_USERNAME): cv.string,
         vol.Required(CONF_PASSWORD): cv.string,
@@ -18,12 +25,45 @@ SCHEMA = vol.Schema(
 )
 
 
+async def validate_and_persist_auth(
+    hass: HomeAssistant, username: str, password: str, name: str, code: str
+):
+    """Validate that the given inputs are correct and persist access token."""
+    _LOGGER.debug("Validation IOLITE auth")
+
+    web_session = async_get_clientsession(hass)
+    oauth_handler = AsyncOAuthHandler(username, password, web_session)
+    access_token = await oauth_handler.get_access_token(code, name)
+
+    expires_at = time.time() + access_token["expires_in"]
+    access_token.update({"expires_at": expires_at})
+    store = hass.helpers.storage.Store(STORAGE_VERSION, STORAGE_KEY)
+    await store.async_save(access_token)
+
+
 class IoliteConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """IOLITE config flow."""
 
     async def async_step_user(self, user_input: Optional[Dict[str, Any]] = None):
         """Invoked when a user initiates a flow via the user interface."""
-        if user_input is None:
-            return self.async_show_form(step_id="user", data_schema=SCHEMA)
 
-        return self.async_create_entry(title="IOLITE", data=user_input)
+        errors: Dict[str, str] = {}
+        if user_input is not None:
+            try:
+                await validate_and_persist_auth(
+                    self.hass,
+                    user_input[CONF_USERNAME],
+                    user_input[CONF_PASSWORD],
+                    user_input[CONF_NAME],
+                    user_input[CONF_CODE],
+                )
+            except Exception as e:
+                errors["base"] = "auth"
+                _LOGGER.error(f"Failed to validate auth: {e}")
+
+            if not errors:
+                return self.async_create_entry(title="IOLITE", data=user_input)
+
+        return self.async_show_form(
+            step_id="user", data_schema=AUTH_SCHEMA, errors=errors
+        )

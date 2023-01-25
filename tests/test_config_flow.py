2b5d8d2c6265f6bac6da2e8a7fed038608f6bc50
Malachi Soord <inverse.chi@gmail.com>
2023-01-25 22:42:32 +0100

    Lint

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -1,5 +1,6 @@
 from unittest.mock import Mock, patch
 
+import pytest
 import voluptuous
 from homeassistant import data_entry_flow
 from homeassistant.config_entries import SOURCE_USER
@@ -7,7 +8,6 @@ from homeassistant.const import CONF_SCAN_INTERVAL
 from homeassistant.core import HomeAssistant
 
 from custom_components.iolite import DOMAIN
-import pytest
 
 
 @pytest.mark.asyncio
@@ -23,7 +23,9 @@ async def test_flow_show_form(hass: HomeAssistant) -> None:
 
 @pytest.mark.asyncio
 @patch("custom_components.iolite.config_flow.validate_and_persist_auth")
-async def test_flow_invalid_scan_interval_range(_validate_and_persist_auth: Mock, hass: HomeAssistant) -> None:
+async def test_flow_invalid_scan_interval_range(
+    _validate_and_persist_auth: Mock, hass: HomeAssistant
+) -> None:
     """Test that the scan interval range is invalid."""
     result = await hass.config_entries.flow.async_init(
         DOMAIN, context={"source": SOURCE_USER}

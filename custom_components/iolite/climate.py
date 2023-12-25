051336e2d6835e006498f386d916cf4aab9ed1ff
Philipp <github@piwo.space>
2023-12-25 14:16:45 +0100

    feat: update for in floor valve

--- custom_components/iolite/climate.py
+++ custom_components/iolite/climate.py
@@ -10,7 +10,7 @@ from homeassistant.const import ATTR_BATTERY_LEVEL, ATTR_TEMPERATURE, TEMP_CELSI
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from iolite_client.client import Client
-from iolite_client.entity import RadiatorValve, Room
+from iolite_client.entity import RadiatorValve, Room, InFloorValve
 
 from . import IoliteDataUpdateCoordinator
 from .const import DOMAIN
@@ -41,6 +41,10 @@ async def async_setup_entry(
             if isinstance(device, RadiatorValve):
                 devices.append(
                     RadiatorValveEntity(coordinator, device, room, coordinator.client)
+                )
+            if isinstance(device, InFloorValve):
+                devices.append(
+                    InFloorValveEntity(coordinator, device, room, coordinator.client)
                 )
 
     for device in devices:
@@ -126,3 +130,73 @@ class RadiatorValveEntity(CoordinatorEntity, ClimateEntity):
         }
 
         return extra_state_attributes
+
+
+class InFloorValveEntity(CoordinatorEntity, ClimateEntity):
+    """Map RadiatorValue to Climate entity."""
+
+    _attr_temperature_unit: str = TEMP_CELSIUS
+    _attr_target_temperature_step: float = 0.5
+    _attr_supported_features: int = SUPPORT_FLAGS
+
+    def __init__(self, coordinator, valve: InFloorValve, room: Room, client: Client):
+        """Initialize the valve."""
+        super().__init__(coordinator)
+        self.valve = valve
+        self.client = client
+        self._attr_unique_id = valve.identifier
+        self._attr_min_temp = TEMP_MIN
+        self._attr_max_temp = TEMP_MAX
+        self._attr_name = f"{self.valve.name} ({room.name})"
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, self._attr_unique_id)},
+            "name": self._attr_name,
+            "manufacturer": self.valve.manufacturer,
+        }
+        self._update_state()
+
+    async def async_set_temperature(self, **kwargs: Any) -> None:
+        """Set new target temperature."""
+        temperature = kwargs.get(ATTR_TEMPERATURE)
+        if temperature is None:
+            return
+
+        await self.client.async_set_temp(self.valve.identifier, temperature)
+        await self.coordinator.async_request_refresh()
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        """Handle updated data from the coordinator."""
+        self._update_state()
+        self.async_write_ha_state()
+
+    @property
+    def hvac_modes(self) -> list[HVACMode]:
+        """Return the list of available operation modes."""
+        return OPERATION_LIST
+
+    @property
+    def room(self) -> Room:
+        """Return device data object from coordinator."""
+        return self.coordinator.data[self.valve.place_identifier]
+
+    @property
+    def hvac_mode(self):
+        """Return hvac target hvac state."""
+        if self._attr_target_temperature == TEMP_MIN:
+            return HVACMode.OFF
+
+        return HVACMode.HEAT
+
+    async def async_set_hvac_mode(self, hvac_mode: HVACMode) -> None:
+        """Set new operation mode."""
+        if hvac_mode == HVACMode.OFF:
+            await self.async_set_temperature(temperature=TEMP_MIN)
+        else:
+            await self.async_set_temperature(temperature=self.target_temperature)
+
+    def _update_state(self):
+        valve: InFloorValve = self.room.devices[self.valve.identifier]
+        self._attr_current_temperature = valve.current_env_temp
+        if self.room.heating:
+            self._attr_target_temperature = self.room.heating.target_temp
